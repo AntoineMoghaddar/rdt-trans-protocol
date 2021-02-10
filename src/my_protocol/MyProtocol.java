a60381690426f27f0c8a23869fa251ce8efa2d38
Antoine Moghaddar <a.a.moghaddar@student.utwente.nl>
2021-02-10 10:57:39 +0100

    test myprot_01 | Moose

--- src/my_protocol/MyProtocol.java
+++ src/my_protocol/MyProtocol.java
@@ -2,6 +2,7 @@ package my_protocol;
 
 import framework.IRDTProtocol;
 import framework.Utils;
+import Utils.Logger;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -29,7 +30,7 @@ public class MyProtocol extends IRDTProtocol {
 
     @Override
     public void sender() {
-        System.out.println("Sending...");
+        Logger.confirm("Sending...");
 
         ArrayList<Integer[]> alldata = new ArrayList<>();
         // read from the input file
@@ -41,28 +42,7 @@ public class MyProtocol extends IRDTProtocol {
         for (int i = 0; i < fileContents.length; i += DATASIZE) {
             alldata.add(Arrays.copyOfRange(fileContents, i, i + DATASIZE));
         }
-//
-//        // keep track of where we are in the data
-//        int filePointer = 0;
-//
-//        // create a new packet of appropriate size
-//        int datalen = Math.min(DATASIZE, fileContents.length - filePointer);
-//        Integer[] pkt = new Integer[HEADERSIZE + datalen];
-//        // write something random into the header byte
-//        pkt[0] = 123;
-//        // copy databytes from the input file into data part of the packet, i.e., after the header
-//        System.arraycopy(fileContents, filePointer, pkt, HEADERSIZE, datalen);
-//
-//        // send the packet to the network layer
-//        getNetworkLayer().sendPacket(pkt);
-//        System.out.println("Sent one packet with header=" + pkt[0]);
-//
-//        // schedule a timer for 1000 ms into the future, just to show how that works:
-//        framework.Utils.Timeout.SetTimeout(1000, this, 28);
 
-        // and loop and sleep; you may use this loop to check for incoming acks...
-
-        loop:
         while (!stop) {
             for (int i = 0; i < PIPESIZE; i++) {
                 HashMap<Boolean, Integer[]> tmp = new HashMap<>();
@@ -76,58 +56,14 @@ public class MyProtocol extends IRDTProtocol {
                 getNetworkLayer().sendPacket(pkt);
             }
         }
-
     }
 
     @Override
     public void TimeoutElapsed(Object tag) {
-        int z = (Integer) tag;
-        // handle expiration of the timeout:
-        System.out.println("Timer expired with tag=" + z);
     }
 
     @Override
     public Integer[] receiver() {
-        System.out.println("Receiving...");
-
-        // create the array that will contain the file contents
-        // note: we don't know yet how large the file will be, so the easiest (but not most efficient)
-        //   is to reallocate the array every time we find out there's more data
-        Integer[] fileContents = new Integer[0];
-
-        // loop until we are done receiving the file
-        boolean stop = false;
-        while (!stop) {
-
-            // try to receive a packet from the network layer
-            Integer[] packet = getNetworkLayer().receivePacket();
-
-            // if we indeed received a packet
-            if (packet != null) {
-
-                // tell the user
-                System.out.println("Received packet, length=" + packet.length + "  first byte=" + packet[0]);
-
-                // append the packet's data part (excluding the header) to the fileContents array, first making it larger
-                int oldlength = fileContents.length;
-                int datalen = packet.length - HEADERSIZE;
-                fileContents = Arrays.copyOf(fileContents, oldlength + datalen);
-                System.arraycopy(packet, HEADERSIZE, fileContents, oldlength, datalen);
-
-                // and let's just hope the file is now complete
-                stop = true;
-
-            } else {
-                // wait ~10ms (or however long the OS makes us wait) before trying again
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    stop = true;
-                }
-            }
-        }
-
-        // return the output file
-        return fileContents;
+        return null;
     }
 }
